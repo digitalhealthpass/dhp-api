e1a020138b6e634785e91b1352a4005317c4852e
Charles Hutcherson <chutcher01@gmail.com>
2023-02-17 09:14:00 -0500

    added x5c to keys

--- helpers/encryption-helper.js
+++ helpers/encryption-helper.js
@@ -6,19 +6,48 @@ const CryptoJS = require("crypto-js");
 const jose = require('node-jose');
 const ECDSA = require('ecdsa-secp256r1')
 const base64url = require('base64url');
-const JSONNormalize = require('json-normalize'); 
+const JSONNormalize = require('json-normalize');
 const moment = require('moment');
 const { v4: uuidv4 } = require('uuid');
+const {jwkThumbprintByEncoding} = require ('jwk-thumbprint');
 
 const constants = require('./constants');
+
+// Inspired by (MIT licensed):
+// https://github.com/hildjj/node-posh/blob/master/lib/index.js
+function certToX5c(cert) {
+    /*
+     * Convert a PEM-encoded certificate to the version used in the x5c element
+     * of a [JSON Web Key](http://tools.ietf.org/html/draft-ietf-jose-json-web-key).
+     *             
+     * `cert` PEM-encoded certificate chain
+     * `maxdepth` The maximum number of certificates to use from the chain.
+     */
+
+    let x5c;
+
+    x5c = cert.replace(/-----[^\n]+\n?/gm, ',').replace(/\n/g, '');
+    x5c = x5c.split(',').filter((c) => {
+        return c.length > 0;
+    });
+
+    return x5c;
+}
 
 const keyPairFromPems = async (publicPem, privatePem) => {
     try {
         const keystore = jose.JWK.createKeyStore();
         await keystore.add(publicPem, 'pem');
         await keystore.add(privatePem, 'pem');
-        return keystore.toJSON(true);
-    } catch(err) {
+        const keyPair = keystore.toJSON(true);
+
+        keyPair.keys[0].x5c = certToX5c(publicPem);
+        delete keyPair.keys[0].x5t;
+
+        keyPair.keys[0]['x5t#S256'] = jwkThumbprintByEncoding(keyPair.keys[0], 'SHA-256', 'base64url');
+        
+        return keyPair;
+    } catch (err) {
         throw new Error(
             `Unable to create key pair from provided certs: ${err.message}`
         );
@@ -32,7 +61,7 @@ const generateKeyPair = () => {
         keyPair.privatePem = privateKey.toPEM();
         keyPair.publicPem = privateKey.asPublic().toPEM();
         return keyPair;
-    } catch(err) {
+    } catch (err) {
         throw new Error(
             `Unable to generate key pair: ${err.message}`
         );
@@ -41,7 +70,7 @@ const generateKeyPair = () => {
 
 const makeProof = (creator) => {
     return {
-        created: `${moment().toISOString().slice(0,-5)}Z`,
+        created: `${moment().toISOString().slice(0, -5)}Z`,
         creator,
         nonce: uuidv4(),
         type: constants.ELLIPTIC_CURVE_TYPE,
@@ -56,9 +85,9 @@ const signCredential = (privateJwk, publicKeyId, credential) => {
     let normalizedCredential = JSONNormalize.normalizeSync(unsignedCredential);
     const signature = privateKey.sign(normalizedCredential);
     normalizedCredential = JSON.parse(normalizedCredential);
-    
+
     normalizedCredential.proof.signatureValue = signature;
-    
+
     return normalizedCredential;
 }
 
